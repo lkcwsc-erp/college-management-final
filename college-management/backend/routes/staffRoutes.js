dbe3b33e118a645892b5f8ac3adf4fb033e9467a
vaidehi80 <vyawharevaidehi@gmail.com>
2026-05-17 15:52:37 +0530

    Add staff login route and refactor imports

--- college-management/backend/routes/staffRoutes.js
+++ college-management/backend/routes/staffRoutes.js
@@ -1,16 +1,63 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getAllStaff, getStaffById, getMyProfile,
-  createStaff, updateStaff, deleteStaff
-} = require('../controllers/staffController');
-const { protect, authorizeRoles } = require('../middleware/authMiddleware');
 
-router.get('/', protect, authorizeRoles('admin'), getAllStaff);
-router.get('/my-profile', protect, authorizeRoles('staff'), getMyProfile);
-router.get('/:id', protect, authorizeRoles('admin'), getStaffById);
-router.post('/', protect, authorizeRoles('admin'), createStaff);
-router.put('/:id', protect, authorizeRoles('admin'), updateStaff);
-router.delete('/:id', protect, authorizeRoles('admin'), deleteStaff);
+const {
+  getAllStaff,
+  getStaffById,
+  getMyProfile,
+  createStaff,
+  updateStaff,
+  deleteStaff,
+  staffLogin
+} = require('../controllers/staffController');
+
+const {
+  protect,
+  authorizeRoles
+} = require('../middleware/authMiddleware');
+
+router.post('/login', staffLogin);
+
+router.get(
+  '/',
+  protect,
+  authorizeRoles('admin'),
+  getAllStaff
+);
+
+router.get(
+  '/my-profile',
+  protect,
+  authorizeRoles('staff'),
+  getMyProfile
+);
+
+router.get(
+  '/:id',
+  protect,
+  authorizeRoles('admin'),
+  getStaffById
+);
+
+router.post(
+  '/',
+  protect,
+  authorizeRoles('admin'),
+  createStaff
+);
+
+router.put(
+  '/:id',
+  protect,
+  authorizeRoles('admin'),
+  updateStaff
+);
+
+router.delete(
+  '/:id',
+  protect,
+  authorizeRoles('admin'),
+  deleteStaff
+);
 
 module.exports = router;
