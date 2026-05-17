49ea691a211d37d619edb0b8f15bba0bea65a180
mayur9423 <mr.karan9423@gmail.com>
2026-05-17 19:04:32 +0530

    Update StudentDashboard.js

--- college-management/frontend/src/pages/dashboard/StudentDashboard.js
+++ college-management/frontend/src/pages/dashboard/StudentDashboard.js
@@ -1,4 +1,4 @@
-
+import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
