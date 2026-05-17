37cfd5bed0765a70fd5339e0a68ec195cb77dfe9
mayur9423 <mr.karan9423@gmail.com>
2026-05-17 13:45:35 +0530

    Update Home.js

--- college-management/frontend/src/pages/Home.js
+++ college-management/frontend/src/pages/Home.js
@@ -271,7 +271,7 @@ const Home = () => {
                   }}
                 >
                   <option value="">Select Gender</option>
-                  <option value="Male">Female</option>
+                  <option value="Male">Male</option>
                   <option value="Female">Female</option>
                   <option value="Other">Other</option>
                 </select>
