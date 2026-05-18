fb4f7c4d09a4dede5bf812210997f8054c6230b4
vaidehi80 <vyawharevaidehi@gmail.com>
2026-05-18 22:56:07 +0530

    Update Admissions.jsx

--- college-management/frontend/src/pages/Admissions.jsx
+++ college-management/frontend/src/pages/Admissions.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import API from '../api/axios';
