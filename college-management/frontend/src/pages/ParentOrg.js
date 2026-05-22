f93cae2bfd89bf640c86f8f4e87504dee2f0b118
Bhagyshri-web <bhagyashrijoshi164@gmail.com>
2026-05-22 14:55:30 +0530

    Fix import statement for VNSSLogo

--- college-management/frontend/src/pages/ParentOrg.js
+++ college-management/frontend/src/pages/ParentOrg.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./About.css";
 import "./ParentOrg.css";
-import VNSSLogo from "../assets/Sanshta_logo.jpeg"; // place logo in src/assets/
+import VNSSLogo from "../assets/Sanshta_logo.jpeg";
 
 const founders = [
   {
