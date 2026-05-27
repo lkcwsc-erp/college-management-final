25b637258d2b9e9049badb375a3a7d544d6d91e8
vaidehi80 <vyawharevaidehi@gmail.com>
2026-05-27 15:05:57 +0530

    Update PrincipalDashboard.jsx

--- college-management/frontend/src/pages/dashboard/PrincipalDashboard.jsx
+++ college-management/frontend/src/pages/dashboard/PrincipalDashboard.jsx
@@ -99,7 +99,6 @@ const AdmissionModal = ({ adm, onClose, onRefresh, showMsg }) => {
           <Field label="Sub-Caste" value={adm.subCaste} />
           <Field label="Religion" value={adm.religion} />
           <Field label="Nationality" value={adm.nationality} />
-          <Field label="Mother Tongue" value={adm.motherTongue} />
           <Field label="Aadhar No." value={adm.aadharNumber} />
           <Field label="APAR / ABC ID" value={adm.aparIdNumber} />
           <Field label="Is Married" value={adm.isMarried ? 'Yes' : 'No'} />
@@ -130,14 +129,12 @@ const AdmissionModal = ({ adm, onClose, onRefresh, showMsg }) => {
           <Field label="Has Caste Validity" value={adm.hasCasteValidity ? 'Yes' : 'No'} />
         </Section>
 
-        {/* Parents */}
         <Section title="👨‍👩‍👧 Parents Information">
           <Field label="Father's Name" value={adm.fatherName} />
-          <Field label="Father's Phone" value={adm.fatherPhone} />
-          <Field label="Father's Occupation" value={adm.fatherOccupation} />
           <Field label="Mother's Name" value={adm.motherName} />
-          <Field label="Mother's Phone" value={adm.motherPhone} />
-          <Field label="Annual Income" value={adm.annualIncome} />
+          <Field label="Guardian Name" value={adm.guardianFullName} />
+          <Field label="Guardian Phone" value={adm.guardianPhone} />
+          <Field label="Annual Income" value={adm.familyIncome} />
         </Section>
 
         {/* Documents */}
