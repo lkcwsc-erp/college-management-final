0e0c421708c750a391a85850ac56459746928354
vaidehi80 <vyawharevaidehi@gmail.com>
2026-06-03 00:31:48 +0530

    Update ExamSectionDashboard.js

--- college-management/frontend/src/pages/dashboard/ExamSectionDashboard.js
+++ college-management/frontend/src/pages/dashboard/ExamSectionDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import API from '../../api/axios';
