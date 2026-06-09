eb914d19b90dd5f7a8a2a9fb3393805df1acf561
Bhagyshri-web <bhagyashrijoshi164@gmail.com>
2026-06-09 13:04:45 +0530

    Update student route authorization roles

--- college-management/backend/routes/studentRoutes.js
+++ college-management/backend/routes/studentRoutes.js
@@ -9,8 +9,8 @@ const { protect, authorizeRoles } = require('../middleware/authMiddleware');
 router.get('/', protect, authorizeRoles('admin', 'staff', 'staff_student', 'staff_principal', 'staff_accounts', 'staff_exam', 'staff_scholarship'), getAllStudents);
 router.get('/my-profile', protect, authorizeRoles('student'), getMyProfile);
 router.get('/:id', protect, authorizeRoles('admin', 'staff', 'staff_student', 'staff_principal', 'staff_accounts', 'staff_exam', 'staff_scholarship'), getStudentById);
-router.post('/', protect, authorizeRoles('admin'), createStudent);
-router.put('/:id', protect, authorizeRoles('admin', 'staff'), updateStudent);
-router.delete('/:id', protect, authorizeRoles('admin'), deleteStudent);
+router.post('/', protect, authorizeRoles('admin', 'staff_principal'), createStudent);
+router.put('/:id', protect, authorizeRoles('admin', 'staff', 'staff_principal'), updateStudent);
+router.delete('/:id', protect, authorizeRoles('admin', 'staff_principal'), deleteStudent);
 
 module.exports = router;
