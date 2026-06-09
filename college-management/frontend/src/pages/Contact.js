d7902af61c2b1bce9acfc44285d5fed73f4e4379
Bhagyshri-web <bhagyashrijoshi164@gmail.com>
2026-06-09 22:13:17 +0530

    Update email address in Contact.js

--- college-management/frontend/src/pages/Contact.js
+++ college-management/frontend/src/pages/Contact.js
@@ -109,7 +109,7 @@ const Contact = () => {
                 <span className="contact-icon">✉️</span>
                 <div>
                   <h4>Email</h4>
-                  <p>lkcwscgkd@gmail.com.edu.in</p>
+                  <p>ilkcwscgkd@gmail.com</p>
                 </div>
               </div>
 
